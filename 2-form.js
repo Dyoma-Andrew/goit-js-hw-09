9b6f7f86a0145bb75275fa1cb572c28b2380278d
Dyoma-Andrew <helmyt12@gmail.com>
2026-07-12 12:21:56 +0000

    Deploying to gh-pages from @ Dyoma-Andrew/goit-js-hw-09@48da888876e07479db44787d2c749e1d68e5ed21 🚀

--- 2-form.js
+++ 2-form.js
@@ -1,14 +1,2 @@
-import"./assets/modulepreload-polyfill-B5Qt9EMX.js";/* empty css                      */const r=document.querySelector(".container"),m=`
-  <form class="feedback-form" autocomplete="off">
-    <label class="form-label">
-      Email
-      <input class="form-input" type="email" name="email" autofocus />
-    </label>
-    <label class="form-label">
-      Message
-      <textarea class="form-textarea" name="message" rows="8"></textarea>
-    </label>
-    <button class="form-btn" type="submit">Submit</button>
-  </form>
-`;r.innerHTML=m;const t=document.querySelector(".feedback-form"),o="feedback-form-state",e={email:"",message:""};function n(){try{const a=localStorage.getItem(o);if(a){const s=JSON.parse(a);e.email=s.email||"",e.message=s.message||"",t.elements.email.value=e.email,t.elements.message.value=e.message}}catch(a){console.error("Error parsing localStorage data:",a)}}n();t.addEventListener("input",a=>{const{name:s,value:l}=a.target;e[s]=l.trim(),localStorage.setItem(o,JSON.stringify(e))});t.addEventListener("submit",a=>{if(a.preventDefault(),!e.email||!e.message){alert("Fill please all fields");return}console.log("Submitted Data:",e),localStorage.removeItem(o),e.email="",e.message="",t.reset()});
+import"./assets/modulepreload-polyfill-B5Qt9EMX.js";/* empty css                      */const t=document.querySelector(".feedback-form"),s="feedback-form-state",e={email:"",message:""};function l(){try{const a=localStorage.getItem(s);if(a){const r=JSON.parse(a);e.email=r.email||"",e.message=r.message||"",t.elements.email.value=e.email,t.elements.message.value=e.message}}catch(a){console.error("Error parsing localStorage data:",a)}}l();t.addEventListener("input",a=>{const{name:r,value:o}=a.target;e[r]=o.trim(),localStorage.setItem(s,JSON.stringify(e))});t.addEventListener("submit",a=>{if(a.preventDefault(),!e.email||!e.message){alert("Fill please all fields");return}console.log("Submitted Data:",e),localStorage.removeItem(s),e.email="",e.message="",t.reset()});
 //# sourceMappingURL=2-form.js.map
